9153a2fc7b60aeac08cdf50a9ed5ae9cd545dd27
Ira S <iryna.shevchenko2@gmail.com>
2023-08-07 08:14:00 +0300

    tasks completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parcedData = JSON.parse(savedData);
+ 
+form.addEventListener("input", throttle(saveMessage, 500));
+form.addEventListener("submit", onSubmit);
+
+updateOutput();
+
+function saveMessage(event) {
+    const objectText = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectText)); 
+};
+
+
+console.log(parcedData);
+
+function updateOutput() {
+    if (savedData) {
+    form.elements.email.value = parcedData.email || '';
+    form.elements.message.value = parcedData.message || '';
+  }
+
+}
+
+updateOutput();
+
+function onSubmit(event) {
+  event.preventDefault();
+
+    const beforeSubmitValues = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+ }
+console.log(beforeSubmitValues);
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  event.currentTarget.reset();
+  objectText = {};
+}
